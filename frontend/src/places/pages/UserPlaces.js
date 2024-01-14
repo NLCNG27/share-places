739e1a7a38d6e9ef4db2e638791ad8f5772729eb
Nguyen Lam <nguyen@cngsoftware.com>
2024-01-14 03:04:03 -0600

    added updating, deleting places, and final stage

--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -1,43 +1,52 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-import PlaceList from '../components/PlaceList';
-import Button from '../../shared/components/FormElements/Button';
-
-const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most famous sky scrapers in the world!',
-        imageUrl: 'https://images.unsplash.com/photo-1428366890462-dd4baecf492b?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        address: '20 W 34th St., New York, NY 10001',
-        location: {
-            lat: 40.750639,
-            lng: -73.985699,
-        },
-        creator: 'u1'
-
-    },
-    {
-        id: 'p2',
-        title: 'Emp. State Building',
-        description: 'One of the most famous sky scrapers in the world!',
-        imageUrl: 'https://images.unsplash.com/photo-1428366890462-dd4baecf492b?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        address: '20 W 34th St., New York, NY 10001',
-        location: {
-            lat: 40.750639,
-            lng: -73.985699,
-        },
-        creator: 'u2'
-    }
-]
+import PlaceList from "../components/PlaceList";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import { useHttpClient } from "../../shared/hooks/http-hook";
 
 const UserPlaces = () => {
+    const [loadedPlaces, setLoadedPlaces] = useState();
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
     const userId = useParams().userId;
-    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
 
-    return <PlaceList items={loadedPlaces} />
+    useEffect(() => {
+        const fetchPlaces = async () => {
+            try {
+                const responseData = await sendRequest(
+                    `http://localhost:5001/api/places/user/${userId}`
+                );
+                setLoadedPlaces(responseData.places);
+            } catch (err) {}
+        };
+        fetchPlaces();
+    }, [sendRequest, userId]);
+
+    const placeDeletedHandler = (deletedPlaceId) => {
+        setLoadedPlaces((prevPlaces) =>
+            prevPlaces.filter((place) => place.id !== deletedPlaceId)
+        );
+    };
+
+    return (
+        <React.Fragment>
+            <ErrorModal error={error} onClear={clearError} />
+            {isLoading && (
+                <div className="center">
+                    <LoadingSpinner asOverlay />
+                </div>
+            )}
+            {!isLoading && loadedPlaces && (
+                <PlaceList
+                    items={loadedPlaces}
+                    onDeletePlace={placeDeletedHandler}
+                />
+            )}
+            ;
+        </React.Fragment>
+    );
 };
 
 export default UserPlaces;
